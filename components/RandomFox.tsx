fa3ae0e7168202d736cf508c065b5a36ea24b01b
Karla Sequen <karlasequen@gmail.com>
2023-08-02 18:21:02 -0600

    add component randomFox

---
+++ components/RandomFox.tsx
@@ -0,0 +1,9 @@
+
+const random = () => Math.floor(Math.random() * 123) + 1;
+
+export const RandomFox = (): JSX.Element => {
+
+    const image = `https://randomfox.ca/images/${random()}.jpg`;
+
+    return <img width={320} height="auto" src={image} className="rounded" />
+}
