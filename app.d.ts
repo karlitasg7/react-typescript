1f0f364cd34b6566775aa9dfc35c271b1bb6b1c0
Karla Sequen <karlasequen@gmail.com>
2023-08-02 20:26:38 -0600

    add global type

---
+++ app.d.ts
@@ -0,0 +1,1 @@
+type IImageItem = { id: string; url: string };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,13 @@ import { MouseEventHandler, useState } from 'react';
 const random = () => Math.floor(Math.random() * 123) + 1;
 const generateId = () => Math.random().toString(36).substring(2, 9);
 
-type ImageItem = { id: string; url: string };
-
 export default function Home() {
 
-  const [images, setImages] = useState<Array<ImageItem>>([]);
+  const [images, setImages] = useState<Array<IImageItem>>([]);
 
   const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
 
-    const newImageItem: ImageItem = {
+    const newImageItem: IImageItem = {
       id: generateId(),
       url: `https://randomfox.ca/images/${random()}.jpg`
     };
