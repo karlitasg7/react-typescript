33dfc8105036741e714827b2f15290521eb559a4
Karla Sequen <karlasequen@gmail.com>
2023-08-02 19:13:22 -0600

    button to add new fox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,40 @@
 
 import Image from 'next/image'
 import { RandomFox } from '../../components/RandomFox';
-import { useState } from 'react';
+import { MouseEventHandler, useState } from 'react';
 
 const random = () => Math.floor(Math.random() * 123) + 1;
-const generateId = () => Math.random().toString(36).substr(2, 9);
+const generateId = () => Math.random().toString(36).substring(2, 9);
 
 type ImageItem = { id: string; url: string };
 
 export default function Home() {
 
-  const [images, setImages] = useState<Array<ImageItem>>([
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
-  ]);
+  const [images, setImages] = useState<Array<ImageItem>>([]);
+
+  const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+
+    const newImageItem: ImageItem = {
+      id: generateId(),
+      url: `https://randomfox.ca/images/${random()}.jpg`
+    };
+
+    setImages([
+      ...images,
+      newImageItem
+    ]);
+  }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       Hello world
+
+      <button
+        onClick={addNewFox}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+      >
+        Add new fox
+      </button>
 
       {images.map(({ id, url }) => (
         <div key={id} className="p-4">
