7f7021c0affe48ae45cfbd769a1b3e74ab008720
Karla Sequen <karlasequen@gmail.com>
2023-08-02 18:39:10 -0600

    add imagesList to show on page

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,9 +1,7 @@
 
-const random = () => Math.floor(Math.random() * 123) + 1;
+type Props = { image: string };
 
-export const RandomFox = (): JSX.Element => {
-
-    const image = `https://randomfox.ca/images/${random()}.jpg`;
+export const RandomFox = ({ image }: Props): JSX.Element => {
 
     return <img width={320} height="auto" src={image} className="rounded" />
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import Image from 'next/image'
 import { RandomFox } from '../../components/RandomFox';
+import { useState } from 'react';
+
+const random = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello world
 
-      <RandomFox />
+      {images.map((image, index) => (
+        <div key={index} className="p-4">
+          <RandomFox image={image} />
+        </div>
+      ))}
+
     </main>
   )
 }
